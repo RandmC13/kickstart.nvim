edb4b2d868adeab1abea90b3ed04267f29ab6461
RandmC13 <75956767+RandmC13@users.noreply.github.com>
2024-06-14 03:37:07 +0100

    Added signature help

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -92,6 +92,8 @@ return { -- LSP Configuration & Plugins
         -- Opens a popup that displays documentation about the word under your cursor
         --  See `:help K` for why this keymap.
         map('K', vim.lsp.buf.hover, 'Hover Documentation')
+        -- Opens a popup that displays information about the symbols under your cursor in insert mode
+        vim.keymap.set({ 'i', 's' }, '<M-k>', vim.lsp.buf.signature_help, { desc = 'Show signature help' })
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
