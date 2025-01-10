27b1028a6284ff432fbab485650a6b9e6aaf594e
RandmC13 <75956767+RandmC13@users.noreply.github.com>
2025-01-10 21:55:57 +0000

    Fixed neorg icon

---
+++ lua/kickstart/plugins/devicons.lua
@@ -0,0 +1,10 @@
+return {
+  'nvim-tree/nvim-web-devicons',
+  opts = { override_by_extension = {
+    ['norg'] = {
+      icon = ' ',
+      name = 'Neorg',
+      color = '#3798d9',
+    },
+  } },
+}
